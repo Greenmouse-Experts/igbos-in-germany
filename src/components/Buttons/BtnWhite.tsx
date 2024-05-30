094a0c22ce52c9b4b368290abd78b53af8315838
iammoses <inawo.moses@gmail.com>
2024-05-30 14:13:44 +0100

    build fix

--- src/components/Buttons/BtnWhite.tsx
+++ src/components/Buttons/BtnWhite.tsx
@@ -1,6 +1,6 @@
 import { MdArrowOutward } from "react-icons/md";
 
-function BtnWhite({color}:{color:string}) {
+function BtnWhite({color}:{color?:string}) {
   return (
     <button className={`${color === "green" ?"bg-colorPrimary btn-green text-white": "btn-white bg-white"}  text-colorPrimary w-fit mt-10 px-8 py-3 font-medium btn  rounded-xl transition-all text-lg flex items-center gap-3`}>
       Join Us Today{" "}
