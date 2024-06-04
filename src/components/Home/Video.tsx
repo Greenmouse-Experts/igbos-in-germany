6ee2d41dc9181de35f8aacdae7abdac155380ac5
iammoses <inawo.moses@gmail.com>
2024-06-04 09:47:37 +0100

    text correction

--- src/components/Home/Video.tsx
+++ src/components/Home/Video.tsx
@@ -4,7 +4,7 @@ function Video() {
   return (
     <section className="flex flex-col justify-center items-center gap-10 ">
       <h4 className=" font-semibold text-black">
-        Igbo Convention Stuttgait Official Video
+        Igbo Convention Stuttgart Official Video
       </h4>
       <div className="xl:w-[80%] w-full md:h-[34rem] h-[24rem] mx-auto flex justify-center ">
         <ReactPlayer url="https://youtu.be/wMPbkF1ORi4?si=-NCsJVI8r0cdiArQ"
