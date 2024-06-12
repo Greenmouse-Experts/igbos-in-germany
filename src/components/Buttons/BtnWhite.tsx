cc4fd57fe7b6d6c7e584fff6baee171360b5f493
iammoses <inawo.moses@gmail.com>
2024-06-12 11:32:44 +0100

    updated

--- src/components/Buttons/BtnWhite.tsx
+++ src/components/Buttons/BtnWhite.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 function BtnWhite({color}:{color?:string}) {
   return (
-    <NavLink to="signin" className={`${color === "green" ?"bg-colorPrimary btn-green text-white": "btn-white bg-white"}  text-colorPrimary w-fit mt-10 px-8 py-3 w-fit font-medium btn  rounded-xl transition-all text-lg flex items-center gap-3`}>
+    <NavLink to="/signin" className={`${color === "green" ?"bg-colorPrimary btn-green text-white": "btn-white bg-white"}  text-colorPrimary w-fit mt-10 px-8 py-3 w-fit font-medium btn  rounded-xl transition-all text-lg flex items-center gap-3`}>
       Join Us Today{" "}
       <span>
         <MdArrowOutward size={20} />
