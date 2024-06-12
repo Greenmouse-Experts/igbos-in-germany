c717a5411442d3419d8f8d92c9205b7abea13e49
iammoses <inawo.moses@gmail.com>
2024-06-12 11:33:17 +0100

    updated

--- src/components/Buttons/BtnPrimary.tsx
+++ src/components/Buttons/BtnPrimary.tsx
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 
 function BtnPrimary() {
   return (
-    <NavLink to="history" className='bg-colorPrimary text-white mt-10 px-8 py-3 font-medium btn btn-green rounded-xl transition-all text-lg w-fit flex items-center gap-3'>
+    <NavLink to="/history" className='bg-colorPrimary text-white mt-10 px-8 py-3 font-medium btn btn-green rounded-xl transition-all text-lg w-fit flex items-center gap-3'>
 Learn more <span><MdArrowOutward size={20} />
 </span>
     </NavLink>
